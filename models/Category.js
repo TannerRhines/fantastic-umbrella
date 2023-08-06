c9d0161209c0abcaefab71f8c29db142a2617026
Tanner Rhines <tannerrhines@protonmail.com>
2023-08-06 01:24:49 -0800

    created Category class id/ category name, added comments

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,23 @@ class Category extends Model {}
 
 Category.init(
   {
+
+    
     // define columns
+
+    // id will be an integer, cannot be null
+
+    // primary key ensures all values are unique, and autoIncrement automatically increases the id value for each new data entry
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
