4c197d3ca034be11dd4ac87cd78c595e97b6e8e7
Tanner Rhines <tannerrhines@protonmail.com>
2023-08-06 01:12:49 -0800

    added comments to server.js

--- server.js
+++ server.js
@@ -12,11 +12,23 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
+
+
+
+
+
 // sync sequelize models to the database, then turn on the server
+
 // app.listen(PORT, () => {
 //   console.log(`App listening on port ${PORT}!`);
 // });
 
+// commented out and replace with sequelize.sync ^
+
+
+
+
+// if force is set to true, sequelize will drop existing tables and recreate them, which can lead to data loss
 
 sequelize.sync({ force: false }).then(() => {
   console.log(`App listening on port ${PORT}!`);
