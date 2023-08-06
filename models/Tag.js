bb7f643ff94c7c2cb00917dbea53d6abcbf4885d
Tanner Rhines <tannerrhines@protonmail.com>
2023-08-06 01:43:54 -0800

    created Tag class in tag.js, added comments

--- models/Tag.js
+++ models/Tag.js
@@ -6,7 +6,25 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+
+    // id will be a number, cannot be null
+
+    // primary key ensures all values are unique 
+    
+    // autoIncrement automatically increases the id value for each new data entry
+
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    // tag name must be a string 
+
+    tag_name: {
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
